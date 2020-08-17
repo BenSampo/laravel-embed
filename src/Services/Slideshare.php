94872fe01ef0e5a9d7d5a64a3ec431ba846afe94
Ben Sampson <ben@sampo.co.uk>
2020-08-17 09:28:14 +0100

    Added slideshare service

---
+++ src/Services/Slideshare.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace BenSampo\Embed\Services;
+
+use DOMDocument;
+use Illuminate\Support\Str;
+use BenSampo\Embed\ServiceBase;
+use Illuminate\Support\Facades\Http;
+
+class Slideshare extends ServiceBase
+{
+    public static function detect(string $url): bool
+    {
+        return Str::startsWith($url, [
+            'http://www.slideshare.net',
+            'https://www.slideshare.net',
+        ]);
+    }
+
+    protected function viewData(): array
+    {
+        return [
+            'iframeUrl' => $this->iframeUrl(),
+        ];
+    }
+
+    protected function iframeUrl(): string
+    {
+        $OEmbedHtml = Http::get("https://www.slideshare.net/api/oembed/2?url={$this->url}&format=json")->json()['html'];
+
+        $dom = new DOMDocument;
+        $dom->loadHTML($OEmbedHtml);
+        return $dom->getElementsByTagName('iframe')[0]->getAttribute('src');
+    }
+}
---
+++ tests/Services/SlideshareTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BenSampo\Embed\Tests\Services;
+
+use BenSampo\Embed\Services\Slideshare;
+use BenSampo\Embed\Tests\Cases\ServiceTestCase;
+
+class SlideshareTest extends ServiceTestCase
+{
+    protected function serviceClass(): string {
+        return Slideshare::class;
+    }
+    
+    protected function expectedViewName(): string {
+        return 'slideshare';
+    }
+
+    protected function expectedViewData(): array {
+        return [
+            'iframeUrl' => 'https://www.slideshare.net/slideshow/embed_code/key/6PCWPGFw9SwsAY',
+        ];
+    }
+
+    protected function validUrls(): array
+    {
+        return [
+            'http://www.slideshare.net/haraldf/business-quotes-for-2011',
+            'https://www.slideshare.net/haraldf/business-quotes-for-2011',
+        ];
+    }
+}
