62b6aa34bc87519a4d4d49b95ec4d518c047226d
Ben Sampson <ben@sampo.co.uk>
2020-08-21 10:32:31 +0100

    Tidy responsive wrapper CSS

--- resources/views/components/responsive-wrapper.blade.php
+++ resources/views/components/responsive-wrapper.blade.php
@@ -1,7 +1,10 @@
 @once
-    <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style>
+    <style>
+        .laravel-embed__responsive-wrapper { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } 
+        .laravel-embed__responsive-wrapper iframe, .laravel-embed__responsive-wrapper object, .laravel-embed__responsive-wrapper embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }
+    </style>
 @endonce
 
-<div class="embed-container">
+<div class="laravel-embed__responsive-wrapper">
     {{ $slot }}
 </div>
