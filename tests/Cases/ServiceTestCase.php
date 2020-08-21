c343c385e6262b950d9f7e60e77d2402ba7b37aa
Ben Sampson <ben@sampo.co.uk>
2020-08-21 18:13:25 +0100

    Formatting

--- tests/Cases/ServiceTestCase.php
+++ tests/Cases/ServiceTestCase.php
@@ -6,8 +6,8 @@ use BenSampo\Embed\ServiceContract;
 
 abstract class ServiceTestCase extends ApplicationTestCase
 {
-    abstract protected function serviceClass(): string
-    ;
+    abstract protected function serviceClass(): string;
+    
     abstract protected function expectedViewName(): string;
 
     abstract protected function expectedViewData(): array;
