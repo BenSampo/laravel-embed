2badb4bddb069fc5f81c3f8d5d7b068625cf44f0
Ben Sampson <ben@sampo.co.uk>
2020-08-22 11:50:35 +0100

    Cache the service returned from the service factory per URL

--- src/ServiceFactory.php
+++ src/ServiceFactory.php
@@ -5,6 +5,7 @@ namespace BenSampo\Embed;
 use BenSampo\Embed\ValueObjects\Url;
 use Symfony\Component\Finder\Finder;
 use BenSampo\Embed\Services\Fallback;
+use Illuminate\Support\Facades\Cache;
 use BenSampo\Embed\Tests\Fakes\ServiceFactoryFake;
 use BenSampo\Embed\Exceptions\ServiceNotFoundException;
 
@@ -16,9 +17,16 @@ class ServiceFactory
     public static function getByUrl(Url $url): ServiceContract
     {
         $factory = self::resolve();
+        $cacheKey = 'larevel-embed-service::' . $url;
+
+        if (Cache::has($cacheKey)) {
+            $serviceClass = Cache::get($cacheKey);
+            return new $serviceClass($url);
+        }
 
         foreach ($factory->serviceClasses() as $serviceClass) {
             if ($serviceClass::detect($url)) {
+                Cache::forever($cacheKey, $serviceClass);
                 return new $serviceClass($url);
             };
         }
