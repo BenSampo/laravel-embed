ded310cdd273ba20634d1fc35e12a62157cd6d44
Jeremy Amos <jeremya@markerseven.com>
2023-07-06 09:49:44 -0700

    support unlisted video url format

--- resources/views/services/vimeo.blade.php
+++ resources/views/services/vimeo.blade.php
@@ -1,3 +1,3 @@
 <x-embed-responsive-wrapper :aspect-ratio="$aspectRatio">
-    <iframe src="https://player.vimeo.com/video/{{ $videoId }}" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+    <iframe src="https://player.vimeo.com/video/{{ $videoId }}@if($videoHash)?h={{ $videoHash}}@endif" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
 </x-embed-responsive-wrapper>
