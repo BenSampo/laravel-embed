9f4ad84c8ecedbe5e5eb91e852c8cdf7a1d030b4
jamosmk7 <jeremya@markerseven.com>
2023-08-11 03:13:18 -0700

    support unlisted video url format (#19)
    * support unlisted video url format
    
    * fix for parsing when no hash is present
    
    * make second param non-greedy

--- src/Services/Vimeo.php
+++ src/Services/Vimeo.php
@@ -9,25 +9,26 @@ class Vimeo extends ServiceBase
 {
     public static function detect(Url $url): bool
     {
-        return (new self($url))->videoId() !== null;
+        return (new self($url))->parseUrl() !== null;
     }
 
     protected function viewData(): array
     {
-        return [
-            'videoId' => $this->videoId(),
-        ];
+        return $this->parseUrl();
     }
 
     /**
      * @link https://stackoverflow.com/a/16841070/3498182
      */
-    protected function videoId(): ?string
+    protected function parseUrl(): ?array
     {
-        preg_match('/(https?:\/\/)?(www\.)?(player\.)?vimeo\.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/', $this->url, $match);
-
+        preg_match('/(https?:\/\/)?(www\.)?(player\.)?vimeo\.com\/([a-z]*\/)*([0-9]{6,11})\/?([a-z0-9]+)?[?]?.*/', $this->url, $match);
+        
         if (array_key_exists(5, $match)) {
-            return $match[5];
+            return [
+                'videoId' => $match[5],
+                'videoHash' => isset($match[6]) ? $match[6] : NULL
+            ];
         }
 
         return null;
