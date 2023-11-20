e858150d988da31838c364a6fa9700f802cbc674
Hugh Messenger <hugh.messenger@gmail.com>
2023-11-20 15:44:26 -0600

    Put back missing allowfullscreen

--- resources/views/services/dailymotion.blade.php
+++ resources/views/services/dailymotion.blade.php
@@ -4,5 +4,6 @@
             frameborder="0"
             type="text/html"
             src="https://www.dailymotion.com/embed/video/{{ $videoId }}"
+            allowfullscreen
     ></iframe>
 </x-embed-responsive-wrapper>
