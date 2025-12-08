9dbe5f475199f519718484fcc480bb84bb4734d4
Samuel De Backer <sdebacker@gmail.com>
2025-12-08 14:03:34 +0100

    Fix aria-label in youtube.blade.php by removing debug code.

--- resources/views/services/youtube.blade.php
+++ resources/views/services/youtube.blade.php
@@ -1,6 +1,6 @@
 <x-embed-responsive-wrapper :aspect-ratio="$aspectRatio">
     <iframe
-        aria-label="foo {{ $label }}"
+        aria-label="{{ $label }}"
         src="https://www.youtube-nocookie.com/embed/{{ $videoId }}"
         frameborder="0"
         allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
